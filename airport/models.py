76463b9e98c2997f294abbe23752117345733cd2
Alla Kuksa <alla.kuksa92@gmail.com>
2024-07-18 16:46:48 +0300

    add Crew model

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+
+    class Meta:
+        ordering = ("first_name",)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
