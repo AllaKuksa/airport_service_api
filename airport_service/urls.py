d3c81360c39ec91945094bb0630496e0da9ed656
Alla Kuksa <alla.kuksa92@gmail.com>
2024-07-31 17:59:20 +0300

    add TokenAuthentication

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/airport/", include("airport.urls", namespace="airport")),
     path("__debug__/", include("debug_toolbar.urls")),
+    path("api/user/", include("user.urls", namespace="user"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
